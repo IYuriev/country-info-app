dba913ce4a4da0a7c806e22e12d2f1818b1782d3
IYuriev <ilya.yuriev.s@gmail.com>
2025-04-07 14:36:10 +0300

    Feat: add ability to create users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CountryModule } from './country/country.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CountryModule],
+  imports: [CountryModule, UserModule, PrismaModule],
   controllers: [],
   providers: [],
 })
