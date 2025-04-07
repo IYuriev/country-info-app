1be8192b2bbe272ba1129fc41de48f06c3608344
IYuriev <ilya.yuriev.s@gmail.com>
2025-04-07 11:33:52 +0300

    Feat: add startup log with application URL

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.APP_PORT);
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
